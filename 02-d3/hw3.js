f435a96bfacaf4c5a1e98f6017e3926b913ab131
Brianna Forster <briannaforster@gmail.com>
2024-09-19 00:49:38 -0700

    Add color gradient and mouseover event to bar chart

--- 02-d3/hw3.js
+++ 02-d3/hw3.js
@@ -10,6 +10,10 @@ const studentCounts = data.map((d) => { return d.students; });
 
 const height = 200, width = 400, barW = 40, barSpace = 5;
 
+const myColors = d3.scaleLinear()
+    .domain([0, studentCounts.length])
+    .range(['indianred', 'darkred'])
+
 const yS = d3.scaleLinear()
     .domain([0, d3.max(studentCounts)])
     .range([0, height])
@@ -17,7 +21,7 @@ const yS = d3.scaleLinear()
 const xS = d3.scaleBand()
     .domain(d3.range(0, studentCounts.length))
     .range([0, width])
-    .padding(0.2)
+    .padding(0.1)
 
 const graph = d3.select('#chart')
     .append('svg')
@@ -29,8 +33,14 @@ graph.selectAll('rect')
     .data(data)
     .enter()
     .append('rect')
-    .style('fill', 'blue')
+    .style('fill', (_, i) => { return myColors(i) })
     .attr('width', xS.bandwidth)
     .attr('height', d => { return yS(d.students) })
     .attr('x', (_, i) => { return xS(i) })
     .attr('y', (d, _) => { return height - d.students })
+    .on('mouseover', function () {
+        d3.select(this).style('opacity', 0.6)
+    })
+    .on('mouseout', function () {
+        d3.select(this).style('opacity', 1)
+    })
