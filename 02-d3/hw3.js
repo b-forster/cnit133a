2b697ee63296c1fca0a2443b7df46b5cc127e73b
Brianna Forster <briannaforster@gmail.com>
2024-09-19 01:17:06 -0700

    Add x and y axes with values to bar chart

--- 02-d3/hw3.js
+++ 02-d3/hw3.js
@@ -8,11 +8,19 @@ const data = [
 ]
 const studentCounts = data.map((d) => { return d.students; });
 
-const height = 200, width = 400, barW = 40, barSpace = 5;
+const margin = { top: 20, right: 20, bottom: 30, left: 40, }
+const height = 200 - margin.top - margin.bottom,
+    width = 400 - margin.left - margin.right,
+    barW = 40,
+    barSpace = 5;
 
 const myColors = d3.scaleLinear()
     .domain([0, studentCounts.length])
     .range(['indianred', 'darkred'])
+
+const verticalGuide = d3.scaleLinear()
+    .domain([0, d3.max(studentCounts)])
+    .range([height, 0])
 
 const yS = d3.scaleLinear()
     .domain([0, d3.max(studentCounts)])
@@ -25,9 +33,16 @@ const xS = d3.scaleBand()
 
 const graph = d3.select('#chart')
     .append('svg')
-    .attr('width', width)
-    .attr('height', height)
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
     .style('background', 'silver')
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+    .call(d3.axisLeft(yS).scale(verticalGuide))
+
+graph.append('g')
+    .call(d3.axisBottom(xS))
+    .attr('transform', 'translate(0, ' + height + ')')
 
 graph.selectAll('rect')
     .data(data)
@@ -37,7 +52,7 @@ graph.selectAll('rect')
     .attr('width', xS.bandwidth)
     .attr('height', d => { return yS(d.students) })
     .attr('x', (_, i) => { return xS(i) })
-    .attr('y', (d, _) => { return height - d.students })
+    .attr('y', (d, _) => { return height - yS(d.students) })
     .on('mouseover', function () {
         d3.select(this).style('opacity', 0.6)
     })
