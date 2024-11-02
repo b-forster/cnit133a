5e2f558d2b515d19d8bb44eba84d6b749ae0944f
Brianna Forster <briannaforster@gmail.com>
2024-11-01 23:16:24 -0700

    [angular] Set up routing

--- 04-angular/orchard/src/app/app-routing.module.ts
+++ 04-angular/orchard/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
