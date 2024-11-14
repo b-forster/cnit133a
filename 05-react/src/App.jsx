32a0098d10306e2fcfe500cc114910f0dccaa86b
Brianna Forster <briannaforster@gmail.com>
2024-11-14 15:35:05 -0800

    [react] Implement course dropdown, Add button, and functionality for displaying added courses in list

--- 05-react/src/App.jsx
+++ 05-react/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   }
 
   return (
-    <CoursePath courses={COURSES} />
+    <CoursePath courseList={COURSES} />
   )
 }
 
--- 05-react/src/CoursePath.jsx
+++ 05-react/src/CoursePath.jsx
@@ -1,5 +1,47 @@
-function CoursePath({ courses }) {
-    return (<></>);
+import { useState } from "react";
+
+function CoursePath({ courseList }) {
+    const [courseIds, setCourseIds] = useState([]); // ex: ['CNIT 132', 'CNIT 133']
+    const [selectedCourseId, setSelectedCourseIds] = useState(''); // ex: 'CNIT 133'
+
+    function handleChangeCourseSelection(e) {
+        setSelectedCourseIds(e.target.value);
+    }
+
+    function handleAddCourse(e) {
+        e.preventDefault();
+        setCourseIds([...courseIds, selectedCourseId]);
+    }
+
+    const formatCourseName = (courseId) => {
+        return `${courseId} - ${courseList[courseId]?.title}`
+    }
+
+    return (
+        <>
+            <form onSubmit={handleAddCourse}>
+                <select onChange={handleChangeCourseSelection}>
+                    {Object.keys(courseList)
+                        .map(courseId =>
+                            <option
+                                value={courseId}
+                                id={`select-${courseId}`}
+                                key={courseId}
+                            >
+                                {formatCourseName(courseId)}
+                            </option>
+                        )}
+                </select>
+                <button type="submit">Add</button>
+            </form>
+
+            <ul>
+                {courseIds?.map(courseId =>
+                    <li key={courseId} id={`display-${courseId}`}>{formatCourseName(courseId)}</li>
+                )}
+            </ul>
+        </>
+    );
 }
 
 export default CoursePath
