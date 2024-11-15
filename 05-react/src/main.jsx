bc528b155120f46e2891ead072ee7cf416577d0c
Brianna Forster <briannaforster@gmail.com>
2024-11-14 20:20:53 -0800

    [react] Add imports for Bootstrap

--- 05-react/src/main.jsx
+++ 05-react/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './scss/styles.scss'
+import * as bootstrap from 'bootstrap'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
